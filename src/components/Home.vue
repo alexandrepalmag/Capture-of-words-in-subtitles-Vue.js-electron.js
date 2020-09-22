eecf5a76ea28cb6c00ea64d4d44107fd1014d4a6
Alexandre Palma Gonçalves <alepalmag85ti@gmail.com>
2020-09-22 02:12:50 -0300

    develoment of src/components/Home.vue

---
+++ src/components/Home.vue
@@ -0,0 +1,33 @@
+<template>
+ <v-container fluid>
+<div class="cards">
+  <Card v-for="word in groupOfWords" :key="word.name" :name="word.name" :amount="word.amount" />
+</div>
+ </v-container>
+</template>
+
+<script>
+import Card from './Card'
+
+export default {
+  components: { Card },
+  data: function () {
+    return{
+      groupOfWords: [
+          {name: 'Hello', amount:800},
+          {name: 'Hello', amount:800},
+          {name: 'Hello', amount:800},
+          {name: 'Hello', amount:800},
+          {name: 'Hello', amount:800},
+      ]
+    }
+  },
+};
+</script>
+
+<style>
+.cards {
+  display: flex;
+  
+}
+</style>
